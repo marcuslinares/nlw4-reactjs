40d2686d26933999093a0b845c0e02f8ea1e7781
Marcus Linares <marcus.linares@gmail.com>
2021-02-28 19:37:59 -0300

    Example of creating a Button component

---
+++ moveit/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from'./components/Button'
+import './styles/global.css';
+function App() {
+  return (
+    <div>
+      <Button color="red">Botao 1</Button>
+      <Button color="blue">Botao 2</Button>
+      <Button color="green">Botao 3</Button>
+    </div>
+  );
+}
+
+export default App;
