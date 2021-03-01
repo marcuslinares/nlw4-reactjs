9b9acfd09b82e1119f439c0db8cf8690e2b08799
Marcus Linares <marcus.linares@gmail.com>
2021-02-28 22:11:16 -0300

    Completion of the experience bar - class day 1

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,11 +1,10 @@
-import { Button } from'./components/Button'
+import { ExperienceBar } from './components/ExperienceBar';
 import './styles/global.css';
+
 function App() {
   return (
-    <div>
-      <Button color="red">Botao 1</Button>
-      <Button color="blue">Botao 2</Button>
-      <Button color="green">Botao 3</Button>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
